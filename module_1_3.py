23f5d86d54cb32e6ab35e5bbf402f7062428b92a
Николай <dhyeheydddjeujd>
2024-10-04 15:00:45 +0300

    04/10

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Nikolai"
+print('Имя:', name)
+age = 21
+print('Возраст:', age)
+age = 21
+new_age =age + 1
+print( 'Новая эра:' , new_age)
+is_student = True
+print('Является студентом:' , is_student)
